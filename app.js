74393952d494a2a6ed0e8b840b06bec7f852e9e0
zamong25 <wool0602@naver.com>
2022-08-18 15:41:48 +0900

    update app.js

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { Client } = require("pg");
-const dbInfo = require("../DB/dbInfo");
+// const dbInfo = require("../DB/dbInfo");
+const dbInfo = {};
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Success");
+});
 
 app.get("/kor", (req, res) => {
   res.send("안녕하세요");
